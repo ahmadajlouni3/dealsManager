ca1404b431b14c9b3ff0ff540a02d31ae7695a9f
ahmadajlouni3 <bootsetup1@gmail.com>
2024-07-10 17:08:18 +0300

    new version of trades managment

---
+++ script.js
@@ -0,0 +1,11 @@
+// const addDeal = document.getElementsByClassName("addDeals")[0];
+// const cancelAddDeal = document.getElementById("cancelAddDeal");
+// const bgCancelAddDeal = document.getElementsByClassName("bgInsertDeal")[0];
+
+// addDeal.addEventListener("click", ()=>{
+//     bgCancelAddDeal.style.display = "flex";
+// });
+
+// cancelAddDeal.addEventListener("click" , () => {
+//     bgCancelAddDeal.style.display = "none"
+// })
